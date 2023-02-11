f3fff887530f01f31311f2e80c776600e0712d2f
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-02-11 19:58:23 +1100

    nav bar and landing page - current issue: photo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import nav_bar from "./nav_bar/index";
+import "bulma/css/bulma.min.css";
+import J_icon from "./assets/slack.png";
+import portrait from "./assets/portrait2.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <section class="section is-small">
+          <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-start">
+              <a class="navbar-item" href="http://localhost:3000/">
+                <img src={J_icon} width="30" height="100" />
+              </a>
+            </div>
+
+            <div clas="navbar-end">
+              <div class="buttons">
+                <a class="button is-info">
+                  <strong>Contact Me</strong>
+                </a>
+
+                <a class="button is-info">
+                  <strong>Github</strong>
+                </a>
+              </div>
+            </div>
+          </nav>
+        </section>
+      </div>
+
+      <section class="section is-small">
+        <h1 class="has-text-centered has-text-weight-bold is-family-monospace is-size-2">
+          Designer, Full-Stack Developer & Learner
+        </h1>
+        <br></br>
+        <h2 class="subtitle has-text-centered">I like making things</h2>
+      </section>
+
+      <section class="section is-small is-family-monospace">
+        <figure class="image is-128x128">
+          <img src={portrait} />
+        </figure>
+      </section>
+
+      <section class="hero is-large is-info">
+        <div class="hero-body">
+          <p class="title">Text-Hero</p>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/nav_bar/index.js
@@ -0,0 +1,7 @@
+import "bulma/css/bulma.min.css";
+
+const nav_bar = () => {
+  return <button class="button is-success is-outlined">Outlined</button>;
+};
+
+export default nav_bar;
